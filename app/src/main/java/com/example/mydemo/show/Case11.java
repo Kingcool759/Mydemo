602a52d45c39cc3af31bb3d96c2b54b22e05a034
zhaojianwei01 <3058587756@qq.com>
2020-09-15 11:31:13 +0800

    可折叠式标题栏

--- app/src/main/java/com/example/mydemo/show/Case11.java
+++ app/src/main/java/com/example/mydemo/show/Case11.java
@@ -1,16 +1,66 @@
 package com.example.mydemo.show;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.mydemo.R;
+import com.example.mydemo.entity.Fruit;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 public class Case11 extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_case11);
+
+        Fruit fruit = new Fruit("大苹果",R.drawable.starsky);
+        String fruitName = fruit.getName();
+        int fruitImageId = fruit.getImageId();
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout)findViewById(R.id.collsping_Toolbar);
+        ImageView fruitImageView = (ImageView)findViewById(R.id.fruit_image_view);
+        TextView fruitContentText = (TextView)findViewById(R.id.fruit_content_text);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        collapsingToolbar.setTitle(fruitName);
+        fruitImageView.setImageResource(fruitImageId);
+        String fruitContent = generatorFruitContent(fruitName);
+        fruitContentText.setText(fruitContent);
+    }
+    private String generatorFruitContent(String fruitName){
+        StringBuilder fruitContent = new StringBuilder();
+        for (int i=0;i<500;i++){
+            fruitContent.append(fruitName);
+        }
+        return fruitContent.toString();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

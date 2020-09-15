16cfa54d1986b21742c66c408a99cd49da890365
zhaojianwei01 <3058587756@qq.com>
2020-09-15 16:14:36 +0800

    popupwindow变色+圆角处理

--- app/src/main/java/com/example/mydemo/popupwindow/TestPopupWindow.java
+++ app/src/main/java/com/example/mydemo/popupwindow/TestPopupWindow.java
@@ -1,12 +1,17 @@
 package com.example.mydemo.popupwindow;
 
+import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.drawable.BitmapDrawable;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 import com.example.mydemo.R;
@@ -18,11 +23,12 @@ import com.example.mydemo.R;
  */
 public class TestPopupWindow extends PopupWindow {
     TestDataListener mTestDataListener;
+    Context mContext;
     int type = 0;
 
 
     public TestPopupWindow(Context context) {
-        super(context);
+        this.mContext = context;
         View contentView = LayoutInflater.from(context).inflate(R.layout.popup_select_layout, null);
         //封装成CommonPopupWindow类
         setContentView(contentView);
@@ -31,11 +37,21 @@ public class TestPopupWindow extends PopupWindow {
         setFocusable(true);
         setBackgroundDrawable(new BitmapDrawable());
         setOutsideTouchable(true);
-        setOnDismissListener(new PopupWindow.OnDismissListener() {
+        // 收起时渐进恢复背景，从半透明-
+        setOnDismissListener(new OnDismissListener() {
             @Override
             public void onDismiss() {
+                ValueAnimator animator = ValueAnimator.ofFloat(0.5f, 1f).setDuration(500);
+                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        setBackgroundAlpha((Float) animation.getAnimatedValue());
+                    }
+                });
+                animator.start();
             }
         });
+        show(contentView);
 
         //初始化控件
         TextView takePhoto = (TextView)contentView.findViewById(R.id.take_photo);
@@ -68,6 +84,7 @@ public class TestPopupWindow extends PopupWindow {
             dismiss();
         });
     }
+
     //回调事件接口
     public interface TestDataListener {
         void getTestData(int type);
@@ -76,4 +93,28 @@ public class TestPopupWindow extends PopupWindow {
     public void setTestClickaListener(TestDataListener testDataListener) {
         mTestDataListener = testDataListener;
     }
+
+    /**
+     * 设置PopupWindow下面的内容的透明度
+     */
+    private void setBackgroundAlpha(float bgAlpha) {
+        WindowManager.LayoutParams lp = ((Activity) mContext).getWindow().getAttributes();
+        lp.alpha = bgAlpha;
+        ((Activity) mContext).getWindow().setAttributes(lp);
+    }
+    /**
+     * 显示弹窗，过程中伴随背景逐渐变暗。
+     * 不要改变MaskLayout的背景色，否则会是灰色背景的框框整体向上升起的效果。
+     */
+    public void show(View parent) {
+        ValueAnimator animator = ValueAnimator.ofFloat(1, 0.5f).setDuration(500);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                setBackgroundAlpha((Float) animation.getAnimatedValue());
+            }
+        });
+        animator.start();
+    }
+
 }
--- app/src/main/java/com/example/mydemo/show/Case22.java
+++ app/src/main/java/com/example/mydemo/show/Case22.java
@@ -52,4 +52,5 @@ public class Case22 extends AppCompatActivity {
             });
         });
     }
+
 }

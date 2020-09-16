41269fdd6560d1284e68ef03c3f16ba4fafe3b69
zhaojianwei01 <3058587756@qq.com>
2020-09-16 18:17:44 +0800

    字段更新

--- app/src/main/java/com/example/mydemo/show/Case28.java
+++ app/src/main/java/com/example/mydemo/show/Case28.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import com.example.mydemo.R;
 
 public class Case28 extends AppCompatActivity {
-
+    //圆形图片处理
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

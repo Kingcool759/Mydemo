5be1c2a1d066ddb1374b5c0c1f6eb1eab3fbd559
zhaojianwei01 <3058587756@qq.com>
2020-10-15 15:45:50 +0800

    RxBus实现

---
+++ app/src/main/java/com/example/mydemo/blog/ActivityC.java
@@ -0,0 +1,29 @@
+package com.example.mydemo.blog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.mydemo.R;
+import com.example.mydemo.entity.MessageEvent;
+import com.example.mydemo.entity.RxBus;
+
+//用于RxBus实现发布-订阅事件总线，替代EventBus
+public class ActivityC extends AppCompatActivity {
+    private Button btnSendMes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c);
+        initView();
+    }
+    private void initView(){
+        btnSendMes = findViewById(R.id.btnSendMes);
+        btnSendMes.setOnClickListener((View)->{
+            RxBus.getDefault().post(new MessageEvent("接收到了RxBus发布的事件"));
+            finish();
+        });
+    }
+}
--- app/src/main/java/com/example/mydemo/blog/Case43.java
+++ app/src/main/java/com/example/mydemo/blog/Case43.java
@@ -2,15 +2,74 @@ package com.example.mydemo.blog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.mydemo.R;
+import com.example.mydemo.entity.MessageEvent;
+import com.example.mydemo.entity.RxBus;
 
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.ArrayList;
+
+import rx.Subscription;
+import rx.functions.Action1;
+
+//RxBus--EventBus的升级版，可替代EventBus,进行事件发布订阅。
 public class Case43 extends AppCompatActivity {
+    private Button btnIntentC;
+    private TextView tvMessageC;
+    private ArrayList<Subscription> rxBusList = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_case43);
+        initView();
+    }
+    private void initView(){
+        btnIntentC = findViewById(R.id.btnIntentC);
+        tvMessageC = findViewById(R.id.tvMessageC);
+        tvMessageC.setText("千夜零一");
+        btnIntentC.setOnClickListener((View)->{
+            startActivity(new Intent(this,ActivityC.class));
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        receive();
+    }
+
+    private void receive(){
+        Subscription subscription = RxBus.getDefault().toObservable(MessageEvent.class)
+                .subscribe(new Action1<MessageEvent>() {
+                    @Override
+                    public void call(MessageEvent messageEvent) {
+                        tvMessageC.setText(messageEvent.getMessage());
+                    }
+                });
+        rxBusList.add(subscription);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        clearSubscription();
+    }
+    /**
+     * 取消该页面所有订阅
+     */
+    private void clearSubscription() {
+        for (Subscription subscription : rxBusList) {  //
+            if (subscription != null && subscription.isUnsubscribed()) {
+                subscription.unsubscribe();
+            }
+        }
     }
 }
